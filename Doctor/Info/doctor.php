c04edcca810105f83405f43c9dde739e5bd02625
Steve Cai <steve.cai@morningstar.com>
2014-12-18 18:03:35 +0800

    Doctor built

---
+++ Doctor/Info/doctor.php
@@ -0,0 +1,19 @@
+<?php
+  require '../include/common.inc.php';
+  include ROOT.'/include/init.inc.php';
+  $action=$_REQUEST["action"];
+  $model=new XmlModel("doctor","doctor.php");
+  
+  $smarty->assign("MyModule","info");
+
+  if($action=="save"){
+	  if($_REQUEST["primary_id"]!=$SysUser["doctor_id"]){
+		echo "hack";
+	  }
+  }else{
+	$action="edit";
+    $_REQUEST["id"]=$SysUser["doctor_id"];
+  }
+  $model->DefaultShow($smarty,$dbmgr,$action,"doctor",$_REQUEST);
+
+?>
---
+++ Doctor/fileupload.php
@@ -0,0 +1,28 @@
+<?php
+ require 'include/common.inc.php';
+ require ROOT.'/classes/obj/upload.php';
+ $field=$_REQUEST["field"];
+ $module=$_REQUEST["module"];
+ if($module==""){
+ $module="default";
+ $field="upload";
+ }
+ $file=$_FILES[$field];
+ if($file==""){
+	echo "Fils is empty";
+	exit;
+ }
+ $filename=date('ymdHIs').$file["name"];
+ $folder=ROOT."/".$CONFIG['fileupload']['upload_path']."/$module/";
+ if(!file_exists($folder)){
+	mkdir($folder,0777);
+ }
+ $file=new Upload($file,$filename,$folder,true);
+ echo $file->safetyUpload();
+ echo "|~~|".$filename;
+
+
+
+
+
+?>
