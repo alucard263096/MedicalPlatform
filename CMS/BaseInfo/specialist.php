979a4291a0897b318f498fe881907557defe8f29
alucard263096 <alucard263096@126.com>
2015-01-11 22:55:10 +0800

    commit for district,block,speialist

---
+++ CMS/BaseInfo/specialist.php
@@ -0,0 +1,12 @@
+<?php
+  require '../include/common.inc.php';
+  include ROOT.'/include/init.inc.php';
+  $action=$_REQUEST["action"];
+  $model=new XmlModel("specialist","specialist.php");
+  
+	$smarty->assign("MyModule","baseinfo");
+  
+  
+  $model->DefaultShow($smarty,$dbmgr,$action,"specialist",$_REQUEST);
+
+?>
---
+++ CMS/Doctor/service.php
@@ -0,0 +1,15 @@
+<?php
+  require '../include/common.inc.php';
+  include ROOT.'/include/init.inc.php';
+  $action=$_REQUEST["action"];
+  $model=new DoctorXmlModel("doctor.php");
+  
+  $smarty->assign("MyModule","doctor");
+  $smarty->assign("MyMenuId",$menuId."doctor_service");
+  
+  
+  if($action==""){
+	
+  }
+
+?>
