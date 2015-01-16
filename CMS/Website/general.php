fbc9356cc2265a1e07182cc756d3497eb4c441ad
Steve Cai <steve.cai@morningstar.com>
2015-01-16 15:52:15 +0800

    add banner,general management

--- CMS/Website/general.php
+++ CMS/Website/general.php
@@ -5,37 +5,7 @@
   $action=$_REQUEST["action"];
   $model=new GeneralTextXmlModel("general.php");
   
-	$smarty->assign("MyModule","website");
-
-  if($action==""){
-
-	$smarty->assign("MyMenuId","general_list");
-	$model->ShowList($dbmgr,$smarty);
-
-  }else if($action=="search"){
-
-	$model->ShowSearchResult($dbmgr,$smarty,$_REQUEST);
-
-  }else if($action=="add"){
-	if($SysUser["is_admin"]!="Y"){
-		echo "System Error";
-		exit;
-	}
-	$smarty->assign("MyMenuId","general_add");
-	$model->Add($dbmgr,$smarty);
-
-  }else if($action=="edit"){
-	$smarty->assign("MyMenuId","general_add");
-	$model->Edit($dbmgr,$smarty,$_REQUEST["id"]);
-
-  }else if($action=="save"){
-  
-	if($_REQUEST["primary_id"]==""&&$SysUser["is_admin"]!="Y"){
-		echo "System Error";
-		exit;
-	}
-	$result=$model->Save($dbmgr,$_REQUEST,$SysUser["id"]);
-	echo $result;
-
-  }
+  $smarty->assign("MyModule","website");
+  $model->DefaultShow($smarty,$dbmgr,$action,"general",$_REQUEST);
+?>
 ?>
---
+++ dbscript/medicalplatform_dr_tb_banner.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE  IF NOT EXISTS `medicalplatform` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `medicalplatform`;
+-- MySQL dump 10.13  Distrib 5.6.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: medicalplatform
+-- ------------------------------------------------------
+-- Server version	5.6.21-enterprise-commercial-advanced-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `dr_tb_banner`
+--
+
+DROP TABLE IF EXISTS `dr_tb_banner`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dr_tb_banner` (
+  `id` int(11) NOT NULL,
+  `code` varchar(45) NOT NULL COMMENT '代码',
+  `title_sc` varchar(500) NOT NULL COMMENT '标题',
+  `title_tc` varchar(500) NOT NULL,
+  `title_en` varchar(500) NOT NULL,
+  `link` varchar(500) NOT NULL COMMENT '超链接',
+  `pic` varchar(500) NOT NULL COMMENT '图片',
+  `remarks` varchar(500) NOT NULL COMMENT '说明',
+  `created_date` datetime NOT NULL,
+  `created_user` int(11) NOT NULL,
+  `updated_date` datetime NOT NULL,
+  `updated_user` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='广告管理';
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-01-16 15:51:26
