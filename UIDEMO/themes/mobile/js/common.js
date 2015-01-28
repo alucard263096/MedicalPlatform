3ede09d70fe18df61a12f5c1fa208b5d570a5460
lexter <wullk@163.com>
2015-01-28 18:20:39 +0800

    update vaccine page

--- UIDEMO/themes/mobile/js/common.js
+++ UIDEMO/themes/mobile/js/common.js
@@ -1,26 +1,4 @@
-﻿$(function () {
-    $("#home-slides").slidesjs({
-        navigation: false,
-        //play: {
-        //    interval: 5000,
-        //    auto: true
-        //},
-        //effect: {
-        //    slide: {
-        //        speed: 500
-        //    },
-        //    fade: {
-        //        speed: 300,
-        //        crossfade: true
-        //    }
-        //}
-    });
-
-    //setVaccineRecommendHeight("vaccineRecommend_1", "right_1");
-    //setVaccineRecommendHeight("vaccineRecommend_1", "left_1");
-    //setVaccineRecommendHeight("vaccineRecommend_2", "right_2");
-    //setVaccineRecommendHeight("vaccineRecommend_2", "left_2");
-});
+﻿
 
 //function setVaccineRecommendHeight(parentId, childId) {
 //    var heightValue = $("#" + parentId).height();
