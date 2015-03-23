0d63ecd5fe0a10614947e54206d6e152f1d8ac44
alucard263096 <alucard263096@126.com>
2015-03-24 00:42:59 +0800

    commit upload excel 30%

---
+++ DoctorCMS/Appointment/batchvalidation.php
@@ -0,0 +1,16 @@
+<?php
+  require '../include/common.inc.php';
+  include ROOT.'/include/init.inc.php';
+  $action=$_REQUEST["action"];
+  
+	$smarty->assign("MyModule","appointment");
+		$smarty->assign("MyMenuId","batchvalidation");
+
+  if($action=="edit"){
+
+  }else{
+	$smarty->display(ROOT.'/templates/appointment/batchvalidation.html');
+  }
+  
+
+?>
