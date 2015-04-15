0d3b90c12f7babb028020cda517b5d4938f39b6c
Steve Cai <steve.cai@morningstar.com>
2015-04-15 12:39:39 +0800

    1. 疫苗预约：把可预约的时间设定在“从今天起3天后至2个月内”。 3. 增加字段：”诊所地址导航“。

--- UI/update.php
+++ UI/update.php
@@ -2,8 +2,8 @@
 
   require 'include/common.inc.php';
 
-  $sql="ALTER TABLE `medicalplatform`.`dr_tb_member_question` 
-ADD COLUMN `read` VARCHAR(1) NULL AFTER `replied_date`;
+  $sql="ALTER TABLE `medicalplatform`.`dr_tb_office_lang` 
+ADD COLUMN `addressnav` VARCHAR(1000) NULL AFTER `open_hour`;
 ";
 	$query = $dbmgr->query($sql);
 
