a4620d6cc876e8301ff1d806a1228202637035d6
Steve Cai <steve.cai@morningstar.com>
2015-05-22 12:29:56 +0800

    fix a bug for without login in notify alipay

--- UI/payment/notify_alipay.php
+++ UI/payment/notify_alipay.php
@@ -10,15 +10,13 @@
   require ROOT.'/classes/datamgr/order.cls.php';
   require ROOT.'/classes/paymentmgr/alipay.cls.php';
 
-  require ROOT.'/include/login.inc.php';
-  $member=$_SESSION[SESSIONNAME]["Member"];
 
   $alipay=new AlipayMgr();
   $ret=$alipay->notify();
 
-  $info=$orderMgr->getGeneAppointment($member["id"],0,$ret["out_trade_no"]);
+  $info=$orderMgr->getGeneAppointment(0,0,$ret["out_trade_no"]);
   if($ret["result"]=="SUCCESS"){
-	$orderMgr->updateGeneAppointmentPayment($member["id"],$info["id"],$ret["trade_no"]);
+	$orderMgr->updateGeneAppointmentPayment($info["member_id"],$info["id"],$ret["trade_no"]);
   }
 
 ?>
