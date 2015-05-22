b434706141cdccf995c7bdba37dbd0c76fb5c85f
Steve Cai <steve.cai@morningstar.com>
2015-05-22 17:07:30 +0800

    update set

--- UI/update.php
+++ UI/update.php
@@ -3,9 +3,7 @@
   require 'include/common.inc.php';
 
   
-  $sql="ALTER TABLE `medicalplatform`.`dr_tb_member_gene_order` 
-ADD COLUMN `express_com` VARCHAR(45) NULL AFTER `report_express_no`,
-ADD COLUMN `report_express_com` VARCHAR(45) NULL AFTER `express_com`;
+  $sql="update dr_tb_member_gene_order set status='K' where id=34
 
 ";
 	$query = $dbmgr->query($sql);
