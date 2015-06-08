94a541d2627e1d261bd3c52fc21a3998eb3dcdde
Steve Cai <steve.cai@morningstar.com>
2015-06-08 18:15:09 +0800

    mail to and get agent

--- UI/test1.php
+++ UI/test1.php
@@ -1,9 +1,4 @@
 <?php
-$value = "my cookie value";
-// 发送一个 24 小时候过期的 cookie
-setcookie("TestCookie",$value);
-
-
-setcookie("loginedmobile", "13751082562");
+$agent = strtolower($_SERVER['HTTP_USER_AGENT']);
 
 ?>
